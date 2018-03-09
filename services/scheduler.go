296f96f2b5268da94ed50320f54fd9da76fc567b
Brian Hoehne <brianhoehne@gmail.com>
2018-03-09 12:45:20 -0700

    add half season scheduler

--- services/scheduler.go
+++ services/scheduler.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/go-pg/pg"
+	randtools "github.com/thebho/random-tools"
 )
 
 // Scheduler takes a season name and an slice of teams and returns and stores the season in the database
@@ -44,24 +45,83 @@ func createMatchUps(teams []model.Team) []model.Match {
 	return matches
 }
 
-type scheduleHelperTeam struct {
-	team               model.Team
-	matchWeekScheduled bool
-	matches            []model.Match
+type MatchUpHelper struct {
+	teamA int
+	teamB int
 }
 
-func createSchedulerHelperArray(teams []model.Team) map[string]scheduleHelperTeam {
-	var scheduleHelpers = make(map[string]scheduleHelperTeam)
-	for i := range teams {
-		scheduleHelpers[teams[i].Abv] = scheduleHelperTeam{team: teams[i]}
+func createNumberOfTeamsSlice(numberOfTeams int) []int {
+	var allTeams = make([]int, numberOfTeams, numberOfTeams)
+	for i := 0; i < numberOfTeams; i++ {
+		allTeams[i] = i
 	}
-	return scheduleHelpers
+	return allTeams
+}
+
+func createMatchUpAlgorithm(numberOfTeams int) {
+	// matchUpMap := make(map[string]bool)
+	// var weeks = make(map[int]MatchUpHelper)
+	for i := 0; i < numberOfTeams/2; i++ {
+		var teamsAvailable = createNumberOfTeamsSlice(numberOfTeams)
+		fmt.Println(teamsAvailable)
+
+	}
+}
+
+// func createMatchUpMatrix(numberOfTeams int) {
+// 	teamsSlice := createNumberOfTeamsSlice(numberOfTeams)
+// }
+
+type twoTeams struct {
+	a int
+	b int
+}
+
+// 1  20
+// 2  19
+// 3  18
+// 4  17
+// 5  16
+// 6  15
+// 7  14
+// 8  13
+// 9  12
+// 10 11
+func newTwoTeams(a, b int) twoTeams { return twoTeams{a: a, b: b} }
+
+func position(pos, offset, totalTeams int) int {
+	if pos+offset < totalTeams {
+		return pos + offset
+	}
+	return ((pos + offset) - totalTeams) + 1
+}
+
+func pos(a, b int) int {
+	return position(a, b, 20)
+}
+
+func createTeamMatrix(teams []int) [][]twoTeams {
+	var teamMatrix = make([][]twoTeams, 19, 19)
+	for i := 0; i < 19; i++ {
+		teamMatrix[i] = []twoTeams{newTwoTeams(teams[0], teams[pos(19, i)]),
+			newTwoTeams(teams[pos(1, i)], teams[pos(18, i)]),
+			newTwoTeams(teams[pos(2, i)], teams[pos(17, i)]),
+			newTwoTeams(teams[pos(3, i)], teams[pos(16, i)]),
+			newTwoTeams(teams[pos(4, i)], teams[pos(15, i)]),
+			newTwoTeams(teams[pos(5, i)], teams[pos(14, i)]),
+			newTwoTeams(teams[pos(6, i)], teams[pos(13, i)]),
+			newTwoTeams(teams[pos(7, i)], teams[pos(12, i)]),
+			newTwoTeams(teams[pos(8, i)], teams[pos(11, i)]),
+			newTwoTeams(teams[pos(9, i)], teams[pos(10, i)]),
+		}
+	}
+	return teamMatrix
 }
 
 func scheduleMatchUps(unscheduledMatches []model.Match, teams []model.Team) []model.Match {
-	_ = createSchedulerHelperArray(teams)
+	// _ = createSchedulerHelperArray(teams)
 	var scheduledMatches []model.Match
-	// rand.RandIndex(count)
-
+	intArray := randtools.Array(len(teams))
+	fmt.Printf("Teams array: %v\n", intArray)
 	return scheduledMatches
 }
--- services/scheduler_test.go
+++ services/scheduler_test.go
@@ -40,8 +40,29 @@ func TestScheduledMatchUps(t *testing.T) {
 	fmt.Println(scheduledMatches)
 }
 
-func TestCreateSchedulerHelperArray(t *testing.T) {
-	teams := teamsHelper(2)
-	helperArray := createSchedulerHelperArray(teams)
-	assert.Equal(t, teams[0].Abv, helperArray[teams[0].Abv].team.Abv, "Team is added to scheduler helper array map")
+// func TestCreateSchedulerHelperArray(t *testing.T) {
+// 	teams := teamsHelper(2)
+// 	helperArray := createSchedulerHelperArray(teams)
+// 	assert.Equal(t, teams[0].Abv, helperArray[teams[0].Abv].team.Abv, "Team is added to scheduler helper array map")
+// }
+
+func TestCreateMatchUpAlgorithm(t *testing.T) {
+	createMatchUpAlgorithm(3)
+}
+func TestPosition(t *testing.T) {
+	assert.Equal(t, 3, position(3, 0, 20))
+	assert.Equal(t, 4, position(3, 1, 20))
+	assert.Equal(t, 2, position(3, 18, 20))
+	assert.Equal(t, 13, position(3, 10, 20))
+	assert.Equal(t, 19, position(19, 0, 20))
+	assert.Equal(t, 1, position(19, 1, 20))
+	assert.Equal(t, 18, position(19, 18, 20))
+	assert.Equal(t, 10, position(19, 10, 20))
+}
+
+func TestCreateTeamMatrix(t *testing.T) {
+	mockTeams := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19}
+	weeks := createTeamMatrix(mockTeams)
+	assert.Equal(t, twoTeams{0, 1}, weeks[1][0])
+	assert.Equal(t, twoTeams{8, 9}, weeks[18][9])
 }
